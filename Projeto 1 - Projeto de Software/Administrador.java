c933422380cc1b1339536cdfad1f6356073f78e1
Henrique Ferreira Marciano <henrique.marciano271@gmail.com>
2025-10-01 10:59:44 -0300

    Update Administrador.java

--- Projeto 1 - Projeto de Software/Administrador.java
+++ Projeto 1 - Projeto de Software/Administrador.java
@@ -1,18 +1,14 @@
-// Administrador também herda de Usuario
+// Administrador herda de Usuario
 public class Administrador extends Usuario {
 
-    // 1. Construtor
+    //Construtor
     public Administrador(int id, String nome, String email, String senha) {
-        // A única tarefa do construtor é chamar o construtor da classe mãe (Usuario)
-        // para inicializar os atributos herdados.
+        //Chamar o construtor da classe mãe (Usuario) para inicializar os atributos herdados.
         super(id, nome, email, senha);
     }
-
-    // 2. Métodos específicos da responsabilidade do Administrador
     
     public void cadastrarDrone(Drone novoDrone) {
-        // Em um sistema real, este método adicionaria o drone a uma lista ou banco de dados.
-        // Aqui, vamos apenas simular a ação com uma mensagem no console.
+        //simular a ação com uma mensagem no console.
         System.out.println("O administrador '" + this.getNome() + "' cadastrou um novo drone: " + novoDrone.getModelo() + " (ID: " + novoDrone.getId() + ").");
     }
 
@@ -21,10 +17,7 @@ public class Administrador extends Usuario {
     }
     
     public void cadastrarUsuario(Usuario novoUsuario) {
-        // Este método poderia criar tanto novos administradores quanto operadores
+        //Cadastra novos usuarios caso necessario
         System.out.println("O administrador '" + this.getNome() + "' cadastrou um novo usuário: " + novoUsuario.getNome() + ".");
     }
-
-    // 3. Não há atributos específicos, então não precisamos de Getters/Setters novos.
-    //    Todos os getters/setters de Usuario (getNome(), getEmail(), etc.) são herdados automaticamente.
 }
