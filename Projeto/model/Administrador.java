eaf0130a49931224d4577e3714bd1a20ed56cb2e
Henrique Ferreira Marciano <henrique.marciano271@gmail.com>
2025-11-25 13:34:59 -0300

    parte de banco de dados feita

---
+++ Projeto/model/Administrador.java
@@ -0,0 +1,24 @@
+package model;
+// Administrador herda de Usuario
+public class Administrador extends Usuario {
+
+    //Construtor
+    public Administrador(int id, String nome, String email, String senha) {
+        //Chamar o construtor da classe mãe (Usuario) para inicializar os atributos herdados.
+        super(id, nome, email, senha);
+    }
+    
+    public void cadastrarDrone(Drone novoDrone) {
+        //simular a ação com uma mensagem no console.
+        System.out.println("O administrador '" + this.getNome() + "' cadastrou um novo drone: " + novoDrone.getModelo() + " (ID: " + novoDrone.getId() + ").");
+    }
+
+    public void cadastrarArea(AreaAgricola novaArea) {
+        System.out.println("O administrador '" + this.getNome() + "' cadastrou uma nova área: " + novaArea.getLocalizacao() + " (ID: " + novaArea.getId() + ").");
+    }
+    
+    public void cadastrarUsuario(Usuario novoUsuario) {
+        //Cadastra novos usuarios caso necessario
+        System.out.println("O administrador '" + this.getNome() + "' cadastrou um novo usuário: " + novoUsuario.getNome() + ".");
+    }
+}
